1ec24f05ffe7ce63042503d0efe87bd2221eb1aa
Narix Hine <127665924+NarixHine@users.noreply.github.com>
2026-07-13 21:16:54 +0800

    Add Vercel Functions for database pooling
    Integrate @vercel/functions to manage database connection pooling with Prisma. Attach the database pool using vercel's attachDatabasePool function and add a postinstall script to automatically generate the Prisma client on dependency installation.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,13 @@
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from './generated/prisma/client'
 import pg from 'pg'
+import { attachDatabasePool } from '@vercel/functions'
 
 const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
 })
-
+attachDatabasePool(pool)
 const adapter = new PrismaPg(pool)
-
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
